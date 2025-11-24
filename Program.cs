51c0a1cf913c50959038a9f877b33549d7c19e57
Khuram Naseem Abbasi <khuramabbasi006@gmail.com>
2025-11-24 14:34:59 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace VersionedDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Versioned Demo App is running!");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("VersionedDemoApp")]
+[assembly: AssemblyDescription("Demo project for DLL versioning")]
+[assembly: AssemblyCompany("Khuram")]
+[assembly: AssemblyProduct("VersionedDemoApp")]
+
+// These will be replaced automatically by GitHub Workflow
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
